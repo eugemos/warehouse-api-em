386917d8e1671ac27f157d041e1b52a61e3b510b
Евгений Москалянов <emos@inbox.ru>
2024-10-15 07:21:52 +0300

    Исправил путь

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from .product import router as product_router
 
 router = APIRouter()
-router.include_router(product_router, prefix='/product')
+router.include_router(product_router, prefix='/products')
