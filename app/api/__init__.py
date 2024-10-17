5a6497526b3e12e24bc975733c93e4e9b434dc07
Евгений Москалянов <emos@inbox.ru>
2024-10-17 21:21:26 +0300

    Добавлены теги для роутеров.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,5 +4,5 @@ from .order import router as orders_router
 from .product import router as products_router
 
 router = APIRouter()
-router.include_router(orders_router, prefix='/orders')
-router.include_router(products_router, prefix='/products')
+router.include_router(orders_router, prefix='/orders', tags=['Orders'])
+router.include_router(products_router, prefix='/products', tags=['Products'])
