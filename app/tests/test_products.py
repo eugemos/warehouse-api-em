26cb27fba9f866a6284f99449fe0dc69a783c2b2
Евгений Москалянов <emos@inbox.ru>
2024-10-20 00:01:45 +0300

    Написаны все тесты для эндпойнта по созданию продукта.

--- app/tests/test_products.py
+++ app/tests/test_products.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Any
 
 from httpx import ASGITransport, AsyncClient
 import pytest
@@ -31,40 +32,74 @@ class TestCreate:
         self, async_client: AsyncClient
     ):
         request_data = self.REQUEST_DATA
-        response_data = await self._send_request(async_client, request_data)
+        response_data = await self._do_request_and_check_response(
+            async_client, request_data, HTTPStatus.CREATED
+        )
         product_created_data = await self._check_product_created_in_db()
         exp_response_data = dict(request_data, id=product_created_data['id'])
-        assert exp_response_data == product_created_data
-        assert exp_response_data == response_data
+        assert product_created_data == exp_response_data
+        assert response_data == exp_response_data
 
-    # async def test_correct_request_without_description_creates_product():
-    #     ...
+    async def test_correct_request_without_description_creates_product(
+        self, async_client: AsyncClient
+    ):
+        request_data = dict(self.REQUEST_DATA)
+        del request_data['description']
+        response_data = await self._do_request_and_check_response(
+            async_client, request_data, HTTPStatus.CREATED
+        )
+        product_created_data = await self._check_product_created_in_db()
+        exp_product_created_data = dict(
+            request_data, description=None, id=product_created_data['id']
+        )
+        exp_response_data = dict(request_data, id=product_created_data['id'])
+        assert product_created_data == exp_product_created_data
+        assert response_data == exp_response_data
 
-    # async def test_request_with_negative_price_fails(self, ):
-    #     ...
+    @pytest.mark.parametrize(
+        'invalid_attribute',
+        [
+            dict(name='price', value=-1),
+            dict(name='amount', value=-1),
+            dict(name='name', value=''),
+            dict(name='name', value='a'*257),
+        ],
+        ids=['price < 0', 'amount < 0', 'empty name', 'name length > 256']
+    )
+    async def test_request_with_invalid_attribute_fails(
+        self, invalid_attribute: dict[str, Any], async_client: AsyncClient
+    ):
+        request_data = dict(self.REQUEST_DATA)
+        request_data[invalid_attribute['name']] = invalid_attribute['value']
+        await self._do_request_and_check_response(
+            async_client, request_data, HTTPStatus.UNPROCESSABLE_ENTITY
+        )
+        await self._check_product_not_created_in_db()
 
-    # async def test_request_with_negative_amount_fails(self, ):
-    #     ...
+    @pytest.mark.parametrize(
+        'omitted_attribute',
+        ['price', 'amount', 'name',],
+    )
+    async def test_request_with_omitted_required_attribute_fails(
+        self, omitted_attribute: str, async_client: AsyncClient
+    ):
+        request_data = dict(self.REQUEST_DATA)
+        del request_data[omitted_attribute]
+        await self._do_request_and_check_response(
+            async_client, request_data, HTTPStatus.UNPROCESSABLE_ENTITY
+        )
+        await self._check_product_not_created_in_db()
 
-    # async def test_request_with_empty_name_fails(self, ):
-    #     ...
-
-    # async def test_request_without_name_fails(self, ):
-    #     ...
-
-    # async def test_request_without_price_fails(self, ):
-    #     ...
-
-    # async def test_request_without_amount_fails(self, ):
-    #     ...
-
-    async def _send_request(
-        self, async_client: AsyncClient, request_data: dict
+    async def _do_request_and_check_response(
+        self,
+        async_client: AsyncClient,
+        request_data: dict,
+        exp_status: HTTPStatus,
     ):
         async with async_client as client:
             response = await client.post("/products", json=request_data)
 
-        assert response.status_code == HTTPStatus.CREATED
+        assert response.status_code == exp_status
         return response.json()
 
     async def _check_product_created_in_db(self) -> dict:
@@ -76,3 +111,9 @@ class TestCreate:
             return schemas.CreateProductResponse.model_validate(
                 product_created, from_attributes=True
             ).model_dump()
+
+    async def _check_product_not_created_in_db(self) -> None:
+        async with AsyncSessionLocal() as session:
+            sqla_result = await session.scalars(select(Product))
+            products_in_db = sqla_result.all()
+            assert len(products_in_db) == 0
