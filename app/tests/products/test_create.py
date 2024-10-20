ca13f15ba22d30b25fe43f677543d850ae85fbcc
Евгений Москалянов <emos@inbox.ru>
2024-10-21 00:29:42 +0300

    Рефакторинг тестов для эндпойнта создания товара.

--- app/tests/products/test_create.py
+++ app/tests/products/test_create.py
@@ -16,51 +16,53 @@ pytestmark = pytest.mark.anyio
 
 
 class TestCreate(EndpointTestCase):
-    REQUEST_DATA = create_product_data('')
+    NORMAL_REQUEST_DATA = create_product_data('')
 
     async def test_correct_full_request_creates_product(
         self, async_client: AsyncClient
     ):
-        request_data = self.REQUEST_DATA
+        request_data = self.NORMAL_REQUEST_DATA
         response_data = await self._do_request_and_check_response(
             async_client, request_data, HTTPStatus.CREATED
         )
         product_created_data = await self._check_product_created_in_db()
-        exp_response_data = dict(request_data, id=product_created_data['id'])
-        assert product_created_data == exp_response_data
+        exp_product_created_data = dict(
+            request_data, id=product_created_data['id']
+        )
+        exp_response_data = exp_product_created_data
+        assert product_created_data == exp_product_created_data
         assert response_data == exp_response_data
 
     async def test_correct_request_without_description_creates_product(
         self, async_client: AsyncClient
     ):
-        request_data = dict(self.REQUEST_DATA)
+        request_data = dict(self.NORMAL_REQUEST_DATA)
         del request_data['description']
         response_data = await self._do_request_and_check_response(
             async_client, request_data, HTTPStatus.CREATED
         )
         product_created_data = await self._check_product_created_in_db()
         exp_product_created_data = dict(
-            request_data, description=None, id=product_created_data['id']
+            request_data, id=product_created_data['id']
         )
-        exp_response_data = dict(request_data, id=product_created_data['id'])
+        exp_response_data = exp_product_created_data
         assert product_created_data == exp_product_created_data
         assert response_data == exp_response_data
 
     @pytest.mark.parametrize(
         'invalid_attribute',
         [
-            dict(name='price', value=-1),
-            dict(name='amount', value=-1),
-            dict(name='name', value=''),
-            dict(name='name', value='a'*257),
+            dict(price=-1),
+            dict(amount=-1),
+            dict(name=''),
+            dict(name='a'*257),
         ],
         ids=['price < 0', 'amount < 0', 'empty name', 'name length > 256']
     )
     async def test_request_with_invalid_attribute_fails(
         self, invalid_attribute: dict[str, Any], async_client: AsyncClient
     ):
-        request_data = dict(self.REQUEST_DATA)
-        request_data[invalid_attribute['name']] = invalid_attribute['value']
+        request_data = dict(self.NORMAL_REQUEST_DATA, **invalid_attribute)
         await self._do_request_and_check_response(
             async_client, request_data, HTTPStatus.UNPROCESSABLE_ENTITY
         )
@@ -73,7 +75,7 @@ class TestCreate(EndpointTestCase):
     async def test_request_with_omitted_required_attribute_fails(
         self, omitted_attribute: str, async_client: AsyncClient
     ):
-        request_data = dict(self.REQUEST_DATA)
+        request_data = dict(self.NORMAL_REQUEST_DATA)
         del request_data[omitted_attribute]
         await self._do_request_and_check_response(
             async_client, request_data, HTTPStatus.UNPROCESSABLE_ENTITY
@@ -91,15 +93,15 @@ class TestCreate(EndpointTestCase):
         )
         return response.json()
 
-    async def _check_product_created_in_db(self) -> dict:
+    async def _check_product_created_in_db(self) -> dict[str, Any]:
         async with AsyncSessionLocal() as session:
             sqla_result = await session.scalars(select(Product))
             products_in_db = sqla_result.all()
             assert len(products_in_db) == 1
             product_created = products_in_db[0]
-            return schemas.CreateProductResponse.model_validate(
+            return schemas.FullProduct.model_validate(
                 product_created, from_attributes=True
-            ).model_dump()
+            ).model_dump(exclude_none=True)
 
     async def _check_product_not_created_in_db(self) -> None:
         async with AsyncSessionLocal() as session:
